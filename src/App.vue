854b31e2abb6d2f9d32ecfa57143b36f82e9929b
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-07-25 13:53:24 -0300

    feat: iniciando projeto vue Cookin UP

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // Importa o componente Banner.vue
+  import Banner from './components/Banner.vue';
+
+  // Exporta o componente principal da aplicação
+  export default {
+    // Define os componentes utilizados no componente principal
+    components: { 
+      // Componente Banner que será utilizado
+      Banner 
+    }
+  }
+</script>
+
+<template>
+  <!-- Renderiza o componente Banner -->
+  <Banner />
+
+  <h1 class="titulo">
+
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
